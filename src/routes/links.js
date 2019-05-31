3f518b352fb493d4c204be88fc2095b8776075a8
Humberto Zapata <humberto.zaleon@gmail.com>
2019-05-31 07:52:53 -0500

    Arreglando rutas post y get de ususarios (prueba) con async/await y transacciones

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,24 +5,60 @@ const pool = require('../database/database');
 
 
 router.get('/', (req, res) => {
-    
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
-    console.log(req.body);
-    res.send('recived link');
+    try{
+        const connection = await pool.getConnection();
 
-    /* pool.getConnection()
-    .then(connection => {
-        connection.beginTransaction();
-        const query = connection.query('SELECT * FROM users');
-        res.status(200).send(query);
-        connection.commit();
-    })
-    .catch(err => {
+        try {
+            const rBody = {
+                title,
+                url,
+                description
+            } = req.body;
+            const newLink = {
+                title: rBody.title,
+                url: rBody.url,
+                description: rBody.description
+            };
+            await connection.query('INSERT INTO links SET ?', [newLink]);
+            res.send('recivido');
+        } finally{
+            pool.releaseConnection(connection);
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.send({error: err, message: err.message});
+    }
 
-    }); */
+    
+
+    // Rutas con async await
+    /* let connection = await pool.getConnection();
+    try {
+        const rBody = {
+            username,
+            password,
+            fullname
+        } = req.body;
+        const newUser = {
+            username: rBody.username,
+            password: rBody.password,
+            fullname: rBody.fullname
+        };
+        await connection.query('INSERT INTO users SET ?', [newUser]);
+        res.send('recivido');
+    } catch (e) {
+        console.log('Error al crear link');
+        throw e;
+    }
+    finally{
+        connection.release();
+    } */
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,65 +4,96 @@ const router = express.Router();
 const pool = require('../database/database');
 
 
-router.get('/', (req, res) => {
-    res.send('get users');
+router.get('/', async (req, res) => {
+
+    const connection = await pool.getConnection();
+    try {
+        connection.beginTransaction();
+
+        const users = await connection.query('SELECT * FROM users');
+        res.send({
+            users
+        });
+        connection.commit();
+
+    } catch (error) {
+        console.log(error);
+        res.send({
+            error: error,
+            message: error.message
+        });
+        connection.rollback();
+    } finally {
+        pool.releaseConnection(connection);
+    }
+
 });
 
-router.post('/', (req, res) => {
-    let conn;
+router.post('/', async (req, res) => {
+    const connection = await pool.getConnection();
     let msj = {
-        ok: '',
-        msj: ''
+        ok: 'true',
+        error: '',
+        msj: 'Usuario creado'
     };
-    pool.getConnection()
-    .then(connection => {
-        conn = connection;
+    try {
         connection.beginTransaction();
-    })
-    .then(() => {
-        const rBody = { username, password, fullname} = req.body;
+        const rBody = {
+            username,
+            password,
+            fullname
+        } = req.body;
         const newUser = {
-        username: rBody.username,
-        password: rBody.password,
-        fullname: rBody.fullname
+            username: rBody.username,
+            password: rBody.password,
+            fullname: rBody.fullname
         };
-        conn.query('INSERT INTO users SET ?', [newUser]);
-    })
-    /* todos los then para mas consultas
-    .then()
-    */
-    .then(() => conn.commit())
-    .then(() => conn.release())
-    .catch(err => {
-        msj.ok = false;
-        msj.msj = 'Error';
-        res.send(msj);
-        conn.rollback();
-        conn.release();
-        throw err;
-    });
-    
-
-    /* const rBody = { username, password, fullname} = req.body;
-    const newUser = {
-        username: rBody.username,
-        password: rBody.password,
-        fullname: rBody.fullname
-    };
-    await pool.query('INSERT INTO links SET ?', [newLink]);
-    console.log(newUser);
-    res.send('recived user'); */
-
-    /* pool.getConnection()
-    .then(connection => {
-        connection.beginTransaction();
-        const query = connection.query('SELECT * FROM users');
-        res.status(200).send(query);
+        await connection.query('INSERT INTO users SET ?', [newUser]);
         connection.commit();
-    })
-    .catch(err => {
+    } catch (error) {
+        connection.rollback();
+        console.log(error);
+        msj.ok = false;
+        msj.error = error;
+        msj.msj = error.message;
+        return res.send(msj);
+    } finally {
+        pool.releaseConnection(connection);
+        return res.send(msj);
+    }
 
-    }); */
+
 });
 
 module.exports = router;
+
+// Rutas con promesas y transacciones
+/* let conn;
+let msj = {
+    ok: '',
+    msj: ''
+};
+pool.getConnection()
+.then(connection => {
+    conn = connection;
+    connection.beginTransaction();
+})
+.then(() => {
+    const rBody = { username, password, fullname} = req.body;
+    const newUser = {
+    username: rBody.username,
+    password: rBody.password,
+    fullname: rBody.fullname
+    };
+    conn.query('INSERT INTO users SET ?', [newUser]);
+})
+.then(() => conn.commit())
+.then(() => conn.release())
+.catch(err => {
+    msj.ok = false;
+    msj.msj = 'Error';
+    res.send(msj);
+    conn.rollback();
+    conn.release();
+    throw err;
+}); */
